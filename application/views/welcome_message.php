566648fd7840738ec53502c06bd89a52c8b021a4
pongpeerawas <peerawat6385@gmail.com>
2017-05-29 13:02:55 +0700

    edit lasted

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -14,7 +14,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	body {
 		background-color: #fff;
 		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
 		color: #4F5155;
 	}
 
